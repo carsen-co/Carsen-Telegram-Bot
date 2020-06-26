e3265acc3db6808f795f647b10204ce2fcaaca53
Mark Melnic <markhappy200@gmail.com>
2020-06-26 22:55:56 +0300

    Search implementation started

--- sv.py
+++ sv.py
@@ -1,25 +1,68 @@
 
+import time
+import threading
 import configparser as cfg
-from main import telegram_bot
+from telegram_bot import telegram_bot
 
+global msg_cfg
+msg_cfg =  "messages.cfg"
 
-def TEST_send_reply(message):
+def TEST_send_reply():
     reply = "Got you, please wait"
     return reply
 
-def start_message(msg_cfg, message):
+def start_message(msg_cfg):
     parser = cfg.ConfigParser()
     parser.read(msg_cfg)
     return parser.get('firstmessage', 'fmsg')
 
-def search(msg_cfg, message):
+class SEARCH():
+    
+    def input_wait(self, msg_cfg):
+        while True:
+            updates = bot.get_updates(offset = update_id)
+            print(updates)
+            updates = updates["result"]
+            if updates:
+                for item in updates:
+                    try:
+                        message = item["message"]["text"]
+                        return message
+                    except:
+                        message = None
+    
+    def manufacturer(self, msg_cfg):
+        parser = cfg.ConfigParser()
+        parser.read(msg_cfg)
+        return parser.get('search', 'manufacturer')
+    
+    def model(self, msg_cfg):
+        parser = cfg.ConfigParser()
+        parser.read(msg_cfg)
+        return parser.get('search', 'model')
+    
+    def __init__(self, msg_cfg):
+        from_user = item["message"]["from"]["id"]
+        bot.send_message(self.manufacturer(msg_cfg), from_user)
+        inp_make = self.input_wait(msg_cfg)
+        
+        update_id = None
+        bot.send_message(self.model(msg_cfg), from_user)
+        while True:
+            inp_model = self.input_wait(msg_cfg)
+            print(inp_make, inp_model)
+            time.sleep(1)
+            if not inp_model == inp_make:
+                break
+
+        print(inp_make, inp_model)
+    
+def error_msg(msg_cfg):
     parser = cfg.ConfigParser()
     parser.read(msg_cfg)
-    manufacturer = parser.get('search', 'manufacturer')
-    model = parser.get('search', 'model')
+    return parser.get('error', 'error_msg')
 
 if __name__=='__main__':
-    msg_cfg = "messages.cfg"
     bot = telegram_bot("config.cfg")
     update_id = None
 
@@ -30,14 +73,18 @@ if __name__=='__main__':
         if updates:
             for item in updates:
                 update_id = item["update_id"]
+                from_user = item["message"]["from"]["id"]
                 try:
                     message = item["message"]["text"]
-                    if message == "search":
-                        reply = search(msg_cfg, message)
-                except:
+                    if message.lower() == "search":
+                        thread = threading.Thread(target = SEARCH, args = ("messages.cfg",))
+                        thread.start()
+                        thread.join()
+                    else:
+                        reply = start_message(msg_cfg)
+                        bot.send_message(reply, from_user)
+                except Exception as e:
+                    print(e)
                     message = None
-                    reply = error_msg(msg_cfg, message)
-                    
-                from_user = item["message"]["from"]["id"]
-                reply = start_message(msg_cfg, message)
-                bot.send_message(reply, from_user)
+                    reply = error_msg(msg_cfg)
+                    bot.send_message(reply, from_user)
