fae4cee9f428dcb4c97a0255c405c1115c12d56c
Mark Melnic <markhappy200@gmail.com>
2020-10-04 21:16:46 +0200

    Changed config handling and parsing

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,42 +3,32 @@ import json
 import requests
 import configparser as cfg
 
+CONFIG_FILE = "config.cfg"
+
 class telegram_bot():
     
     def __init__(self, config):
         self.token = self.read_token_from_config(config)
         self.base = "https://api.telegram.org/bot{}/".format(self.token)
+        self.parser = cfg.ConfigParser()
         
     def get_updates(self, offset = None):
         url = self.base + "getUpdates?timeout=100"
         if offset:
             url = url + "&offset={}".format(offset + 1)
-            
-        r = requests.get(url)
-        return json.loads(r.content)
+        response = requests.get(url)
+        return json.loads(response.content)
     
     def send_message(self, message, chat_id):
         url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, message)
         if message is not None:
             requests.get(url)
-            
-    def read_token_from_config(self, config):
-        parser = cfg.ConfigParser()
-        parser.read(config)
-        return parser.get('credentials', 'token')
 
-    def TEST_send_reply(self):
-        reply = "Got you, please wait"
-        return reply
+    def read_token_from_config(self, config):
+        return self.parser.get('credentials', 'token')
 
     def error_msg(self):
-        msg_cfg =  "messages.cfg"
-        parser = cfg.ConfigParser()
-        parser.read(msg_cfg)
-        return parser.get('error', 'error_msg')
+        return self.parser.get('messages', 'error_msg')
 
     def start_message(self):
-        msg_cfg =  "messages.cfg"
-        parser = cfg.ConfigParser()
-        parser.read(msg_cfg)
-        return parser.get('firstmessage', 'fmsg')
+        return self.parser.get('messages', 'first_message')
