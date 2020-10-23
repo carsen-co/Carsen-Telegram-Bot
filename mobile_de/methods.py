03533260c94b69c129eb3bd93c21a06add14d1b2
Mark Melnic <markhappy200@gmail.com>
2020-10-23 16:03:02 +0200

    Synced with logic

--- mobile_de/methods.py
+++ mobile_de/methods.py
@@ -1,45 +1,53 @@
-import json, scalg
+import scalg
 
 from mobile_de.scraper import *
-from settings import _MAKESJSON, TEST_MAKESJSON
-
 
 # perform a detailed search of each listing
-def search(search_params: list) -> list:
-    makes_dict = load_makes("mobile_de")
-    current_url, pagesnr = search_url(makes_dict, search_params)
+def search(search_params: list, db=False) -> list:
+    if db:
+        current_url, pagesnr, database = search_url(search_params, db)
+    else:
+        current_url, pagesnr = search_url(search_params, db)
 
     # get links
     car_links = []
     for i in range(pagesnr):
-        for link in get_car_links(current_url):
+        for link in get_page_listings(current_url + "&lang=en"):
             car_links.append(link)
         current_url = next_page(current_url, i + 1)
 
     assert len(car_links) != 0
 
-    data = [get_car_data(link) for link in car_links]
-    data = scalg.score_columns(data, [2, 3, 4], [1, 0, 0])
+    data = [get_car_data(link + "&lang=en") for link in car_links]
+    data = scalg.score_columns(data, [2, 3, 4], [0, 1, 0])
 
-    return data
+    if db:
+        return database, data
+    else:
+        return data
 
 
 # perform a surface search for the generated url
-def surface_search(search_params: list) -> list:
-    makes_dict = load_makes("mobile_de")
-    current_url, pagesnr = search_url(makes_dict, search_params)
+def surface_search(search_params: list, db=False) -> list:
+    if db:
+        current_url, pagesnr, database = search_url(search_params, db)
+    else:
+        current_url, pagesnr = search_url(search_params, db)
 
     data = []
     for i in range(pagesnr):
-        for item in surface_data(current_url):
+        for item in surface_data(current_url + "&lang=en"):
             data.append(item)
         current_url = next_page(current_url, i + 1)
 
     assert data != []
 
-    data = scalg.score_columns(data, [2, 3, 4], [1, 0, 0])
+    data = scalg.score_columns(data, [2, 3, 4], [0, 1, 0])
 
-    return data
+    if db:
+        return data, database
+    else:
+        return data
 
 
 # existing searches checker
@@ -47,27 +55,10 @@ def checker(data: list) -> list:
     changed = False
     car_urls = [d[0] for d in data]
     for link in car_urls:
-        new_price = check_car_price(link)
-        if not float(new_price) == float(data[car_urls.index(link)][3]):
+        new_price = check_car_price(link + "&lang=en")
+        if not float(new_price) == float(data[car_urls.index(link)][2]):
             changed = True
-            data[car_urls.index(link)][3] = new_price
+            data[car_urls.index(link)][2] = new_price
 
     assert changed == True
     return data
-
-
-# load makes and models from json file
-def load_makes(website: str) -> dict:
-    try:
-        with open(_MAKESJSON, "r", encoding="utf-8", newline="") as mjson:
-            data = mjson.read()
-            makes_dict = json.loads(data)
-            makes_dict = makes_dict[website]
-        return makes_dict
-
-    except FileNotFoundError:
-        with open(TEST_MAKESJSON, "r", encoding="utf-8", newline="") as mjson:
-            data = mjson.read()
-            makes_dict = json.loads(data)
-            makes_dict = makes_dict[website]
-        return makes_dict
--- mobile_de/scraper.py
+++ mobile_de/scraper.py
@@ -3,15 +3,13 @@ from requests import get
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
 
-from settings import HEADERS
-
-BASE_URL = "https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&scopeId=C&sfmr=false"
+from settings import HEADERS, BASE_URL, MATCH_RATIO, REG_KEYS, PRICE_KEYS, _MDE_MAKES_DICT
 
 
-def search_url(makes, inp: list) -> list:
+def search_url(inp: list, db: bool) -> list:
     # what each makes index is
-    inp[0] = inp[0].lower()  # 0 - make
-    inp[1] = inp[1].lower()  # 1 - model
+    # 0 - make
+    # 1 - model
     # 2 - minprice
     # 3 - maxprice
     # 4 - minreg
@@ -19,49 +17,15 @@ def search_url(makes, inp: list) -> list:
     # 6 - minmileage
     # 7 - maxmileage
 
+    # generate url parameters
     url_params = ""
 
-    # handle make and model
-    car_make = inp[0]
-    car_model = inp[1]
-    if not inp[0].lower() == "any" or not inp[0] == "":
-        make_matcher = []
-        for make in makes:
-            make_matcher.append(
-                SequenceMatcher(a=make["n"].lower(), b=car_make).ratio()
-            )
-            if make["n"].lower() == inp[0]:
-                car_make = str(make["i"])
-                if not inp[1] == "any" or not inp[1] == "":
-                    model_matcher = []
-                    for model in make["models"]:
-                        model_matcher.append(
-                            SequenceMatcher(a=model["m"].lower(), b=car_model).ratio()
-                        )
-                        if model["m"].lower() == inp[1]:
-                            car_model = str(model["v"])
-                            break
-                    if car_model == inp[1] and any(x > 0.6 for x in model_matcher):
-                        car_model = make["models"][
-                            model_matcher.index(max(model_matcher))
-                        ]["v"]
-                break
+    # handle make, model and database name
+    car_make, car_model, database = make_model_matcher(
+        inp[0].lower(), inp[1].lower()
+    )
 
-        if car_make == inp[0] and any(x > 0.6 for x in make_matcher):
-            car_make = makes[make_matcher.index(max(make_matcher))]["i"]
-            model_matcher = []
-            for model in makes[make_matcher.index(max(make_matcher))]["models"]:
-                model_matcher.append(
-                    SequenceMatcher(a=model["m"].lower(), b=car_model).ratio()
-                )
-                if model["m"].lower() == inp[1]:
-                    car_model = str(model["v"])
-                    break
-            if car_model == inp[1] and any(x > 0.6 for x in model_matcher):
-                car_model = makes[make_matcher.index(max(make_matcher))]["models"][
-                    model_matcher.index(max(model_matcher))
-                ]["v"]
-
+    if car_model != "" or car_model != 0:
         url_params += "&makeModelVariant1.makeId=" + car_make
         url_params += "&makeModelVariant1.modelId=" + car_model
     else:
@@ -87,14 +51,14 @@ def search_url(makes, inp: list) -> list:
     if not inp[7] == "" or not inp[7] == 0:
         url_params += "&maxMileage=" + str(inp[7])
 
-    url = BASE_URL + url_params + "&pageNumber=1"
+    url = BASE_URL + url_params + "&pageNumber=1&lang=en"
 
     # check number of pages
     response = get(url, headers=HEADERS)
     soup = BeautifulSoup(response.content, "html.parser")
 
     checker = soup.find(class_="h2 u-text-orange rbt-result-list-headline").get_text()
-    checker = int(checker.split(" ")[0].replace(" ", "").replace(".", ""))
+    checker = int(checker.split(" ")[0].replace(" ", "").replace(",", ""))
 
     pagesnr = soup.find_all(class_="btn btn--muted btn--s")
     if len(pagesnr) == 0:
@@ -102,7 +66,103 @@ def search_url(makes, inp: list) -> list:
     else:
         pagesnr = int(pagesnr[(len(pagesnr) - 1)].get_text())
 
-    return url, pagesnr
+    if db:
+        return url, pagesnr, database
+    else:
+        return url, pagesnr
+
+
+def make_model_matcher(car_make: str, car_model: str) -> list:
+    og = [car_make, car_model]
+    database = ""
+    if not og[0].lower() == "any" or not og[0] == "":
+        make_matcher = []
+        for make in _MDE_MAKES_DICT:
+            make_matcher.append(
+                SequenceMatcher(a=make["n"].lower(), b=car_make).ratio()
+            )
+            if make["n"].lower() == og[0]:
+                car_make = str(make["i"])
+                database += str(make["n"]).replace(" ", "-") + "_"
+                if not og[1] == "any" or not og[1] == "":
+                    model_matcher = []
+                    for model in make["models"]:
+                        model_matcher.append(
+                            SequenceMatcher(a=model["m"].lower(), b=car_model).ratio()
+                        )
+                        if model["m"].lower() == og[1]:
+                            car_model = str(model["v"])
+                            database += str(model["m"]).replace(" ", "-")
+                            break
+                    if car_model == og[1] and any(x > MATCH_RATIO for x in model_matcher):
+                        car_model = make["models"][
+                            model_matcher.index(max(model_matcher))
+                        ]
+                        database = str(car_model["m"]).replace(" ", "-")
+                        car_model = car_model["v"]
+                break
+
+        if car_make == og[0] and any(x > MATCH_RATIO for x in make_matcher):
+            car_make = _MDE_MAKES_DICT[make_matcher.index(max(make_matcher))]
+            database += str(car_make["n"]).replace(" ", "-") + "_"
+            car_make = car_make["i"]
+            model_matcher = []
+            for model in _MDE_MAKES_DICT[make_matcher.index(max(make_matcher))]["models"]:
+                model_matcher.append(
+                    SequenceMatcher(a=model["m"].lower(), b=car_model).ratio()
+                )
+                if model["m"].lower() == og[1]:
+                    car_model = str(model["v"])
+                    database = str(model["m"]).replace(" ", "-")
+                    break
+            if car_model == og[1] and any(x > MATCH_RATIO for x in model_matcher):
+                car_model = _MDE_MAKES_DICT[make_matcher.index(max(make_matcher))]["models"][
+                    model_matcher.index(max(model_matcher))
+                ]
+                database = str(car_model["m"]).replace(" ", "-")
+                car_model = car_model["v"]
+    return car_make, car_model, database
+
+
+def index_db_finder(url: str) -> str:
+    db_indexes = []
+    # find url make id
+    make_sub = "&makeModelVariant1.makeId="
+    if make_sub in url:
+        make_id = []
+        for ch in url[url.find(make_sub) + len(make_sub) :]:
+            try:
+                int(ch)
+                make_id.append(ch)
+            except ValueError:
+                db_indexes.append("".join(make_id))
+                break
+    # find url model id
+    model_sub = "&makeModelVariant1.modelId="
+    if model_sub in url:
+        model_id = []
+        for ch in url[url.find(model_sub) + len(model_sub) :]:
+            try:
+                int(ch)
+                model_id.append(ch)
+            except ValueError:
+                db_indexes.append("".join(model_id))
+                break
+
+    og = [db_indexes[0], db_indexes[1]]
+    database = ""
+    if not og[0] == 0 or not og[0] == "":
+        for make in _MDE_MAKES_DICT:
+            if make["i"] == og[0]:
+                database += str(make["n"]).replace(" ", "-") + "_"
+                if not og[1] == 0 or not og[1] == "":
+                    for model in make["models"]:
+                        if model["v"] == og[1]:
+                            database += str(model["m"]).replace(" ", "-")
+                            break
+                break
+
+    return database
 
 
 def next_page(current_url: str, current_page: int) -> str:
@@ -122,25 +182,35 @@ def surface_data(url: str) -> list:
 
     data = []
     for listing in listings:
-        url = listing["href"]
-        title = listing.find(class_ = "h3 u-text-break-word").get_text()
-        price = listing.find(class_ = "h3 u-block").get_text().replace(u'\xa0', ' ').replace('.', '')[:-2]
+        listing_url = listing["href"]
+        title = listing.find(class_="h3 u-text-break-word").get_text()
+        price = int(
+            listing.find(class_="h3 u-block")
+            .get_text()
+            .replace("\xa0", "")
+            .replace(",", "")
+            .replace(".", "")
+            .replace("€", "")
+        )
 
         # handle mileage and registration
-        regmil = listing.find(class_ = "rbt-regMilPow").get_text().split(",")
+        regmil = listing.find(class_="rbt-regMilPow").get_text().split(",")
         reg = regmil[0]
-        if "Neuwagen" in reg or "Tageszulassung" in reg:
+        if any(keyword in reg for keyword in REG_KEYS):
             reg = datetime.now().year
         else:
-            reg = reg[-4:]
-        mileage = regmil[1].replace(u'\xa0', ' ').replace('.', '')[:-2].replace(' ', '')
+            reg = int(reg[-4:])
+        try:
+            mileage = int(regmil[1].replace(u"\xa0", "").replace(".", "")[:-2])
+        except ValueError:
+            mileage = 0
 
-        data.append([url, title, reg, price, mileage])
+        data.append([listing_url, title, price, reg, mileage])
 
     return data
 
 
-def get_car_links(url: str) -> list:
+def get_page_listings(url: str) -> list:
     response = get(url, headers=HEADERS)
     soup = BeautifulSoup(response.content, "html.parser")
 
@@ -152,7 +222,7 @@ def get_car_links(url: str) -> list:
     ]
 
 
-def get_car_data(url: str) -> list:
+def get_car_data(url: str, find_db=False) -> list:
     response = get(url, headers=HEADERS)
     soup = BeautifulSoup(response.content, "html.parser")
 
@@ -160,27 +230,19 @@ def get_car_data(url: str) -> list:
     car_title = soup.find(id="rbt-ad-title").get_text()
 
     # price
-    car_price = soup.find(class_="h3 rbt-prime-price").get_text().replace(".", "")
-    if "Brutto" in car_price:
-        car_price = int(car_price[:-11])
+    car_price = soup.find(class_="h3 rbt-prime-price").get_text().replace(",", "")
+    if any(keyword in car_price for keyword in PRICE_KEYS):
+        car_price = int(car_price[1:-8])
     else:
-        car_price = int(car_price[:-2])
+        car_price = int(car_price[1:])
 
     # registration
     try:
         car_reg = soup.find(id="rbt-firstRegistration-v").get_text()
     except AttributeError:
         car_reg = soup.find(id="rbt-category-v").get_text()
-    if "Neufahrzeug" in car_reg:
+    if any(keyword in car_reg for keyword in REG_KEYS):
         car_reg = datetime.now().year
-
-    # elif 'Vorführfahrzeug' in car_reg:
-    #    car_reg = 4
-    #    #carReg = 'Demo Car'
-    # elif 'Jahreswagen' in car_reg:
-    #    car_reg = 3
-    #    #carReg = 'Employee Car'
-    #    #Jahreswagen - employee car
     else:
         car_reg = int(car_reg[3:])
 
@@ -188,9 +250,53 @@ def get_car_data(url: str) -> list:
     car_mileage = soup.find(id="rbt-mileage-v").get_text().replace(".", "")[:-3]
 
     # power
-    # car_power = soup.find(id = "rbt-power-v").get_text().split("(")[1][ : -4]
+    try:
+        car_power = soup.find(id="rbt-power-v").get_text().split("(")[1][:-4]
+    except AttributeError:
+        car_power = 0
 
-    return [url, car_title, car_reg, car_price, car_mileage]  # , car_power
+    # vehicle type
+    car_type = soup.find(id="rbt-category-v").get_text().split(",")[0]
+
+    # fuel type
+    fuel_type = soup.find(id="rbt-fuel-v").get_text()
+
+    # transmission
+    transmission = soup.find(id="rbt-transmission-v").get_text().split(" ")[0]
+
+    # car color
+    try:
+        color = soup.find(id="rbt-color-v").get_text()
+    except AttributeError:
+        color = ""
+
+    # options
+    try:
+        options = [
+            option.get_text()
+            for option in soup.find(id="rbt-features").find(class_="g-row")
+        ]
+    except AttributeError:
+        options = []
+
+    data = [
+        url,
+        car_title,
+        int(car_price),
+        int(car_reg),
+        int(car_mileage),
+        car_type,
+        fuel_type,
+        transmission,
+        color,
+        "|".join(options),
+    ]  # , car_power
+
+    if find_db:
+        database = index_db_finder(url)
+        return data, database
+    else:
+        return data
 
 
 def check_car_price(url: str) -> int:
@@ -202,7 +308,8 @@ def check_car_price(url: str) -> int:
     except AttributeError:
         return False
 
-    if "Brutto" in car_price:
-        return int(car_price[:-11])
+    car_price = soup.find(class_="h3 rbt-prime-price").get_text().replace(",", "")
+    if any(keyword in car_price for keyword in PRICE_KEYS):
+        return int(car_price[1:-8])
     else:
-        return int(car_price[:-2])
+        return int(car_price[1:])
