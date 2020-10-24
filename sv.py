58b2941b2cd1b63f86fc1fb7ec93dff62c35415f
Mark Melnic <markhappy200@gmail.com>
2020-10-24 18:41:56 +0200

    Formatting with py black

--- sv.py
+++ sv.py
@@ -1,15 +1,15 @@
-
 import time, threading
 import configparser as cfg
 
 from telegram_bot import telegram_bot
 from mobile_de.methods import surface_search
 
-class SEARCH():
+
+class SEARCH:
     def input_wait(self, offset_update):
         update_id = offset_update
         while True:
-            updates = bot.get_updates(offset = update_id)
+            updates = bot.get_updates(offset=update_id)
             updates = updates["result"]
             if updates:
                 for item in updates:
@@ -20,36 +20,43 @@ class SEARCH():
                         message = ""
                     return message, update_id
 
-    def __init__(self, entry_update):            
-        bot.send_message(bot.parser.get('messages', 'manufacturer'), from_user)
+    def __init__(self, entry_update):
+        bot.send_message(bot.parser.get("messages", "manufacturer"), from_user)
         inp_make, entry_update = self.input_wait(entry_update)
 
-        bot.send_message(bot.parser.get('messages', 'model'), from_user)
+        bot.send_message(bot.parser.get("messages", "model"), from_user)
         inp_model, entry_update = self.input_wait(entry_update)
 
-        bot.send_message(bot.parser.get('messages', 'budget'), from_user)
+        bot.send_message(bot.parser.get("messages", "budget"), from_user)
         inp_budget, entry_update = self.input_wait(entry_update)
         if inp_budget == "":
             inp_budget = ["", ""]
         else:
-            inp_budget = [int(inp_budget)-(int(inp_budget)/10), int(inp_budget)+(int(inp_budget)/10)]
+            inp_budget = [
+                int(inp_budget) - (int(inp_budget) / 10),
+                int(inp_budget) + (int(inp_budget) / 10),
+            ]
 
         bot.send_message("Working, please wait...", from_user)
 
-        chat_input = [inp_make, inp_model, inp_budget[0], inp_budget[1], '', '', '', '']
+        chat_input = [inp_make, inp_model, inp_budget[0], inp_budget[1], "", "", "", ""]
         data = surface_search(chat_input)
 
         scores = [d[5] for d in data]
 
         best = data[scores.index(max(scores))]
-        bot.send_message("Here is the best listing I could find:\n" + best[1] + "\n" + best[0], from_user)
+        bot.send_message(
+            "Here is the best listing I could find:\n" + best[1] + "\n" + best[0],
+            from_user,
+        )
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     bot = telegram_bot()
     update_id = None
 
     while True:
-        updates = bot.get_updates(offset = update_id)
+        updates = bot.get_updates(offset=update_id)
         print(updates)
         updates = updates["result"]
         if updates:
@@ -61,7 +68,7 @@ if __name__=='__main__':
                 message = item["message"]["text"]
                 messages.append(message)
                 if messages[-1].lower() == "search":
-                    thread = threading.Thread(target = SEARCH, args = (update_id, ))
+                    thread = threading.Thread(target=SEARCH, args=(update_id,))
                     thread.start()
                     thread.join()
                 elif stspam != 1:
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,18 @@
-
 import json
 import requests
 import configparser as cfg
 
 CONFIG_FILE = "config.cfg"
 
-class telegram_bot():
+
+class telegram_bot:
     def __init__(self):
         self.parser = cfg.RawConfigParser()
         self.parser.read(CONFIG_FILE)
         self.token = self.read_token_from_config()
         self.base = "https://api.telegram.org/bot{}/".format(self.token)
 
-    def get_updates(self, offset = None):
+    def get_updates(self, offset=None):
         url = self.base + "getUpdates?timeout=100"
         if offset:
             url = url + "&offset={}".format(offset + 1)
@@ -25,10 +25,10 @@ class telegram_bot():
             requests.get(url)
 
     def read_token_from_config(self):
-        return self.parser.get('credentials', 'token')
+        return self.parser.get("credentials", "token")
 
     def error_msg(self):
-        return self.parser.get('messages', 'error_msg')
+        return self.parser.get("messages", "error_msg")
 
     def start_message(self):
-        return self.parser.get('messages', 'first_message')
+        return self.parser.get("messages", "first_message")
